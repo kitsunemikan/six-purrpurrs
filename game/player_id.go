16b80b538d03d1fa5b5d3199ec5615724b3bf6ac
kitsunemikan <108350823+kitsunemikan@users.noreply.github.com>
2022-12-25 10:49:02 +0300

    Added String() method to PlayerID

--- game/player_id.go
+++ game/player_id.go
@@ -18,3 +18,13 @@ func (p PlayerID) Other() PlayerID {
 
 	panic(fmt.Sprintf("PlayerID: get other player: player is invalid (value=%d)", p))
 }
+
+func (p PlayerID) String() string {
+	if p == P1 {
+		return "P1"
+	} else if p == P2 {
+		return "P2"
+	}
+
+	return fmt.Sprintf("UnknownP%d", int(p))
+}
